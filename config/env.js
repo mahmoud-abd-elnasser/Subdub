ab5f9dc27423a38388af21ed26a1175c237c8846
Mahmoud Abd Elnaser <mahmoudabdelnaser248@gmail.com>
2026-06-23 08:42:53 +0300

    implement mongoDB

--- config/env.js
+++ config/env.js
@@ -1,9 +1,9 @@
 import {config} from "dotenv";
 
-const nodeEnv = process.env.NODE_ENV;
+const nodeEnv = process.env.NODE_ENV || 'development';
 if (!nodeEnv) {
     throw new Error("NODE_ENV must be set before loading configuration");
 }
-config({ path: `.env.${nodeEnv}` });
+config({ path: `.env` });
 
-export const { PORT, NODE_ENV } = process.env
+export const { PORT, NODE_ENV, DATABASE_URL } = process.env
